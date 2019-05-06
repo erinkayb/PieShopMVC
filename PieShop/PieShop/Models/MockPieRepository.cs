e1a7a548b080e05a2ff0d9acebce2ee574a891c6
erinkayb <erinkayb@gmail.com>
2019-05-06 12:05:39 +0200

    added styling and pictures

--- PieShop/PieShop/Models/MockPieRepository.cs
+++ PieShop/PieShop/Models/MockPieRepository.cs
@@ -23,26 +23,26 @@ namespace PieShop.Models
                 new Pie()
                 {
                     Id = 1, Name = "Apple Pie", Price = 10.99, ShortDescription = "Warm pie",
-                    LongDescription = "Warm pie made with apples and cinnamon.", ImageThumbnailUrl = null,
-                    ImageUrl = null, IsPieOfTheWeek = true
+                    LongDescription = "Warm pie made with apples and cinnamon.", ImageThumbnailUrl = "https://res.cloudinary.com/erinkayb/image/upload/v1557136935/Apple-pie.jpg",
+                    ImageUrl = "https://res.cloudinary.com/erinkayb/image/upload/v1557136935/Apple-pie.jpg", IsPieOfTheWeek = true
                 },
                 new Pie()
                 {
-                    Id = 1, Name = "Strawberry Cheesecake", Price = 12.50, ShortDescription = "Cold pie",
-                    LongDescription = "Cool pie made with the creamiest cheese and fresh strawberries.", ImageThumbnailUrl = null,
-                    ImageUrl = null, IsPieOfTheWeek = false
+                    Id = 1, Name = "Strawberry Cheesecake", Price = 12.99, ShortDescription = "Cold pie",
+                    LongDescription = "Cool pie made with the creamiest cheese and fresh strawberries.", ImageThumbnailUrl = "https://res.cloudinary.com/erinkayb/image/upload/v1557136936/classic-strawberry-cheesecake.jpg",
+                    ImageUrl = "https://res.cloudinary.com/erinkayb/image/upload/v1557136936/classic-strawberry-cheesecake.jpg", IsPieOfTheWeek = false
                 },
                 new Pie()
                 {
                     Id = 1, Name = "Blueberry Pie", Price = 9.99, ShortDescription = "Warm pie",
-                    LongDescription = "Warm pie made with fresh blueberries.", ImageThumbnailUrl = null,
-                    ImageUrl = null, IsPieOfTheWeek = false
+                    LongDescription = "Warm pie made with fresh blueberries.", ImageThumbnailUrl = "https://res.cloudinary.com/erinkayb/image/upload/v1557136936/Blueberry-Pie.jpg",
+                    ImageUrl = "https://res.cloudinary.com/erinkayb/image/upload/v1557136936/Blueberry-Pie.jpg", IsPieOfTheWeek = false
                 },
                 new Pie()
                 {
-                    Id = 1, Name = "Cream Pie", Price = 10, ShortDescription = "Cold pie",
-                    LongDescription = "Cool, creamy pie made with fresh cream.", ImageThumbnailUrl = null,
-                    ImageUrl = null, IsPieOfTheWeek = false
+                    Id = 1, Name = "Cream Pie", Price = 8.99, ShortDescription = "Cold pie",
+                    LongDescription = "Cool, creamy pie made with fresh cream.", ImageThumbnailUrl = "https://res.cloudinary.com/erinkayb/image/upload/v1557136936/cream-pie.jpg",
+                    ImageUrl = "https://res.cloudinary.com/erinkayb/image/upload/v1557136936/cream-pie.jpg", IsPieOfTheWeek = false
                 },
             };
         }
